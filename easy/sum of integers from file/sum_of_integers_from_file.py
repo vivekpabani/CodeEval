75c54d70ca40cf23230f8bcd6c087510e4d2b29b
vivekpabani <viky.pabani@gmail.com>
2016-03-26 10:31:01 -0500

    sum of integers.

---
+++ easy/sum of integers from file/sum_of_integers_from_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+
+"""
+Problem Definition :
+
+
+"""
+
+__author__ = 'vivek'
+
+import sys
+
+
+def main():
+
+    for i in xrange(1, 100):
+        if i%2 != 0:
+            print i
+
+if __name__ == '__main__':
+    main()
